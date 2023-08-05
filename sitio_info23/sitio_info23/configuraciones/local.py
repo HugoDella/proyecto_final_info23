d1b11c8d02621f7f7063ee8ebfdb36ebfe9d66f6
hugodella <hugodellamea91@hotmail.com>
2023-08-05 15:38:26 -0300

    act setting2

--- sitio_info23/sitio_info23/configuraciones/local.py
+++
@@ -1,8 +0,0 @@
-# #from .settings import *
-
-# # SECURITY WARNING: don't run with debug turned on in production!
-
-# # Database
-# # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-#
-
