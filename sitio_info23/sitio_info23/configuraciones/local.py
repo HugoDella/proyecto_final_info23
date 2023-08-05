7a2905beb96e53af80036bc7575dd0278134214c
hugodella <hugodellamea91@hotmail.com>
2023-08-05 15:22:43 -0300

    act setting.py

--- sitio_info23/sitio_info23/configuraciones/local.py
+++ sitio_info23/sitio_info23/configuraciones/local.py
@@ -1,22 +1,8 @@
-from .settings import *
+# #from .settings import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'sitio_info23db',
-        "USER": "root",
-        "PASSWORD": "Hdd35469880",
-        'HOST': 'localhost',
-        'PORT': 3306,
-    }
-}
+# # Database
+# # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+#
 
